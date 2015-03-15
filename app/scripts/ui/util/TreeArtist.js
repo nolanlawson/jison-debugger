cefd7e6061fd400ddea5dd6cbe86006e3b338202
Nolan Lawson <nolan.lawson@gmail.com>
2015-03-15 01:23:46 -0400

    resize nodes

--- app/scripts/ui/util/TreeArtist.js
+++ app/scripts/ui/util/TreeArtist.js
@@ -25,26 +25,30 @@ function drawAbstractSvg(root) {
   var svgHeight = ((maxDepth + 1) * yIncrement);
   var yPadding = 40;
 
+  var maxChildren = 0;
+
   function calculateSubtreeWidth(node) {
     node.width = 0;
-    if (!node.children) {
-      return;
-    }
-    node.children.forEach(function (child, i) {
-      calculateSubtreeWidth(child);
-      node.width += child.width;
-      if (i > 0) {
-        node.width += xIncrement;
+    if (node.children) {
+      if (node.children.length > maxChildren) {
+        maxChildren = node.children.length;
       }
-    });
+      node.children.forEach(function (child, i) {
+        calculateSubtreeWidth(child);
+        node.width += child.width;
+        if (i > 0) {
+          node.width += xIncrement;
+        }
+      });
+    }
   }
 
   calculateSubtreeWidth(root);
 
-  if (root.width < 200) {
+  if (root.width < 200 || maxChildren > 3) {
     // use a bigger xIncrement so it doesn't look so crowded.
     // recalculate. TODO: make it not recalculate, be smarter about this
-    xIncrement = 120;
+    xIncrement = 100;
     calculateSubtreeWidth(root);
   }
 
